5b0f166552c3d00f9fd42e031ece706fd7dc7232
zollie <zollie@zair.local>
2015-02-26 14:31:18 -0500

    added /consul volume

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --name=consul --restart=always --net=host -v /var/run/docker.sock:/var/run/docker.sock zollie/consul "$@"
+docker run -d --name=consul --restart=always --net=host -v /var/run/docker.sock:/var/run/docker.sock -v /consul zollie/consul "$@"
 
